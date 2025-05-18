d542585b94f24178fc05f1c86ed3d2addd335cba
Bernarden <bernarden@users.noreply.github.com>
2025-05-19 00:03:54 +1200

    Adds types to run.py file.

--- smart_emails/domain/run.py
+++ smart_emails/domain/run.py
@@ -1,7 +1,13 @@
+from datetime import datetime
+from typing import List
+
+from smart_emails.domain.attribute import Attribute
+
+
 class Run:
-	def __init__(self, attributes, date):
+	def __init__(self, attributes: List[Attribute], date: datetime):
 		self.attributes = attributes
 		self.date = date
 
 	def __repr__(self):
-		return "Test run: %s" % (self.attributes)
+		return "Test run: %s" % self.attributes
